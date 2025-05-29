32f517ea3871219f1668be39e73e96246b04fafa
Manav <manavbhatt056@gmail.com>
2025-05-30 02:27:09 +0530

    experiences added, contact form with server actions added with resend api

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import About from "@/components/about";
+import Contact from "@/components/contact";
+import Experience from "@/components/experience";
 
 
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
@@ -12,6 +15,9 @@ export default function Home() {
       <SectionDivider />
       <About />
       <Projects />
+      <Skills/>
+      <Experience/>
+      <Contact/>
 
     </main>
   );
