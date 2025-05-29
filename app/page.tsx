66aa3fbd65ae815ed231d612de8e2bb86334c5cc
Manav <manavbhatt056@gmail.com>
2025-05-29 18:13:48 +0530

    added header, projects, intro, and fixed imports with images

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import About from "@/components/about";
 
-function page() {
+
+import Intro from "@/components/intro";
+import Projects from "@/components/projects";
+import SectionDivider from "@/components/section-divider";
+
+export default function Home() {
   return (
-    <div>page</div>
-  )
-}
+    <main className="flex flex-col items-center px-4">
+      <Intro />
+      <SectionDivider />
+      <About />
+      <Projects />
 
-export default page
+    </main>
+  );
+}
